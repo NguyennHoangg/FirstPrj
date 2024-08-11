e93c1cfe865e28fdb870a9c34d9fe06f2b31cb3e
NguyennHoangg <nguyenhuyhoang270804@gmail.com>
2024-08-11 12:20:20 +0700

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Huy Hoàng");
+        System.out.println("Welcome to the DHKTPM19B ");
+    }
+}
